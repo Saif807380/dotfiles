40a0a1b43ee879f7a8ca9b7ac7e2b0e65da747de
Saif Kazi <saif1204kazi@gmail.com>
2022-04-19 20:52:05 +0530

    Added fish config

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+status is-interactive; and pyenv virtualenv-init - | source
+
+# aliases
+alias ls=lsd
